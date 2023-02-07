58d35498900cbba59dc8676f41a7a85e5a941d3d
CDX <cai-dexing@foxmail.com>
2023-02-07 18:42:32 +0800

    Modify completion keymap

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -9,24 +9,27 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+        },
+        config = function()
+          -- pass { paths = "./my-snippets/"} to load snippets from my-snippets folder
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = "./my-snippets" })
+        end,
+      },
       "saadparwaiz1/cmp_luasnip",
       -- pictograms
       "onsails/lspkind-nvim",
     },
     config = function()
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       local lspkind = require("lspkind")
 
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
             luasnip.lsp_expand(args.body)
           end
@@ -41,20 +44,6 @@ return {
           { name = 'buffer' },
           { name = 'path' },
         }),
-        -- formatting = {
-        --   fields = { 'kind', 'abbr', 'menu' },
-        --   format = function(entry, vim_item)
-        --     local kind = lspkind.cmp_format({
-        --       mode = "symbol_text",
-        --       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-
-        --     })(entry, vim_item)
-        --     local strings = vim.split(kind.kind, "%s", { trimempty = true })
-        --     kind.kind = " " .. (strings[1] or "") .. " "
-        --     kind.menu = "    (" .. (strings[2] or "") .. ")"
-        --     return kind
-        --   end
-        -- },
         formatting = {
           format = lspkind.cmp_format({
             mode = 'symbol_text', -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
@@ -69,30 +58,17 @@ return {
           })
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-u>"] = cmp.mapping.scroll_docs(4),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          -- use `Tab` to complete
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-              cmp.select_next_item()
-              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-              -- they way you will only jump inside the snippet region
+              cmp.confirm({ select = true })
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
             else
               fallback()
             end
@@ -118,18 +94,5 @@ return {
         })
       })
     end,
-  },
-
-  -- snippets
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        -- pass { paths = "./my-snippets/"} to load snippets from my-snippets folder
-        require("luasnip.loaders.from_vscode").lazy_load({ paths = "./my-snippets" })
-      end,
-    },
-    lazy = true,
   },
 }
