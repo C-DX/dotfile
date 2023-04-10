bbab53f4f973e78d15d129a72e3c2784ac4874bc
CDX <cai-dexing@foxmail.com>
2023-04-11 01:18:17 +0800

    Fix: forget the current snippet when leaving the insert mode.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -17,6 +17,19 @@ return {
         config = function()
           -- pass { paths = "./my-snippets/"} to load snippets from my-snippets folder
           require("luasnip.loaders.from_vscode").lazy_load({ paths = "./my-snippets" })
+
+          vim.api.nvim_create_autocmd("ModeChanged", {
+            pattern = "*",
+            callback = function()
+              if
+                ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+                and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
         end,
       },
       "saadparwaiz1/cmp_luasnip",
