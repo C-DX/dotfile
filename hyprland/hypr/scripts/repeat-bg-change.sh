b00e1f933a299a70051b5882312b562ebe60863f
CDX <57952382+C-DX@users.noreply.github.com>
2024-06-04 19:47:47 +0800

    feat(swww): set fps to 120(default: 30).

--- hyprland/hypr/scripts/repeat-bg-change.sh
+++ hyprland/hypr/scripts/repeat-bg-change.sh
@@ -2,6 +2,6 @@
 
 while true; do
 	wallpaper=$(find ~/Downloads/wallpaper/ -type f | shuf --random-source=/dev/urandom -n 1)
-	swww img $wallpaper --transition-type random
+	swww img $wallpaper --transition-type random --transition-fps 120
 	sleep 600
 done
--- hyprland/waybar/scripts/bg-change.sh
+++ hyprland/waybar/scripts/bg-change.sh
@@ -2,4 +2,4 @@
 
 # swww query || swww-daemon
 wallpaper=$(find ~/Downloads/wallpaper/ -type f | shuf --random-source=/dev/urandom -n 1)
-swww img $wallpaper --transition-type random
+swww img $wallpaper --transition-type random --transition-fps 120
