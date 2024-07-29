e4f0e5e3397eba5bd5c5d795f870ef257a8828d6
CDX <57952382+C-DX@users.noreply.github.com>
2024-07-29 15:09:45 +0800

    feat(nvim): git current line blame, move LazyExtras to lazyvim.json

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,12 +12,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.luasnip" },
-    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
-    { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
