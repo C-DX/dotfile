cbc1e92424e90c4390bcf3e8ec64b2c66faea682
DX C <57952382+C-DX@users.noreply.github.com>
2025-04-04 22:26:26 +0800

    feat(nvim): replace `nvim-cmp` with `blink.cmp`.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,9 +3,13 @@
 -- Add any additional options here
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.g.snacks_animate = false
+
+if vim.fn.has("win32") then
+  vim.o.shell = "pwsh"
+end
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h13"
-  vim.g.neovide_transparency = 0
   vim.g.neovide_refresh_rate = 165
 end
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,61 +1,28 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-cmdline" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      require("luasnip").setup({
-        region_check_events = "CursorHold,InsertLeave",
-        -- those are for removing deleted snippets, also a common problem
-        delete_check_events = "TextChanged,InsertEnter",
-      })
-
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-          if cmp.visible() then
-            local entry = cmp.get_selected_entry()
-            if not entry then
-              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            end
-            cmp.confirm()
-          else
-            fallback()
+    "saghen/blink.cmp",
+    opts = {
+      keymap = { preset = "super-tab" },
+      appearance = {
+        nerd_font_variant = "normal",
+      },
+      cmdline = {
+        enabled = true,
+        keymap = { preset = "inherit" },
+        sources = function()
+          local type = vim.fn.getcmdtype()
+          -- Search forward and backward
+          if type == "/" or type == "?" then
+            return { "buffer" }
           end
-        end, { "i", "s", "c" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
+          -- Commands
+          if type == ":" or type == "@" then
+            return { "cmdline" }
           end
-        end, { "i", "s" }),
-      })
-
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = { { name = "buffer" } },
-      })
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline", option = { ignore_cmds = { "Man", "!" } } },
-        }),
-      })
-    end,
+          return {}
+        end,
+        completion = { menu = { auto_show = true } },
+      },
+    },
   },
 }
